d6f02bf47197f135e8b4b3083223770f688b8ba7
hananhi <hananhimoni@gmail.com>
2024-02-02 18:12:37 +0200

    add firebase in the server side

--- Server/server.js
+++ Server/server.js
@@ -6,29 +6,15 @@ import cors from 'cors';
 import leadsRoutes from  './routes/leadsRoutes.js'
 import usersRoutes from  './routes/usersRoutes.js'
 
+import admin from 'firebase-admin';
+import serviceAccount from './firebaseServiceAccount.js';
 dotenv.config();
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDHXR_Ij5tGwudt0O7jQ-2Kql-mWe5YJ-Q",
-  authDomain: "crm-system-99d52.firebaseapp.com",
-  projectId: "crm-system-99d52",
-  storageBucket: "crm-system-99d52.appspot.com",
-  messagingSenderId: "365806722615",
-  appId: "1:365806722615:web:2e04155e380f28e92ae9b2",
-  measurementId: "G-N40LKSDRPM"
-};
-
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 
 const pool = mysql.createPool({
